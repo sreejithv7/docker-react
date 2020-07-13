fb33e05fe85b356ec2ef98b29b44ec5a21361db1
sreejithv7 <sreejithvs1167@gmail.com>
2020-07-13 18:41:14 +0530

    code changed after aws deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Just for change!
+          Changing after deploying in AWS!
         </p>
         <a
           className="App-link"
